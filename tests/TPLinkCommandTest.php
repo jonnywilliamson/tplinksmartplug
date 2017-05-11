7813ab6f139ce686395e3e4cb41bbe8a56cf44ec
Jonathan Williamson <jonnywilliamson@gmail.com>
2017-05-11 03:20:00 +0100

    Use some collection magic in the tests.

--- tests/TPLinkCommandTest.php
+++ tests/TPLinkCommandTest.php
@@ -2,6 +2,7 @@
 
 namespace Williamson\TPLinkSmartplug\tests;
 
+use Illuminate\Support\Collection;
 use ReflectionClass;
 use ReflectionMethod;
 use Williamson\TPLinkSmartplug\TPLinkCommand;
@@ -14,17 +15,17 @@ class TPLinkCommandTest extends \PHPUnit_Framework_TestCase
     public function it_checks_all_commands_returned_are_an_array()
     {
         $reflector = new ReflectionClass(TPLinkCommand::class);
-        $methods = $reflector->getMethods(ReflectionMethod::IS_PUBLIC);
+        $methods = collect($reflector->getMethods(ReflectionMethod::IS_PUBLIC));
 
-        foreach ($methods as $method) {
-            if ($method->getNumberOfParameters() == 0) {
+        $methods
+            ->filter(function (ReflectionMethod $method) {
+                return $method->getNumberOfParameters() === 0;
+            })
+            ->each(function (ReflectionMethod $method) {
                 $name = $method->getName();
                 $this->assertInternalType('array', TPLinkCommand::$name());
-            }
-        }
+            });
     }
-
-    //TODO Methods that require a parameter
 
 }
 
