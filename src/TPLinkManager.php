5c22c571f4ad9fd6d77463f34aa3f62c60e7d207
Shane Rutter <shane@shanerutter.co.uk>
2017-11-29 21:16:39 +0000

    Auto Discovery of TPLinkDevices on network Changes to TPLinkManager to better handle discovered devices

--- src/TPLinkManager.php
+++ src/TPLinkManager.php
@@ -3,13 +3,14 @@
 namespace Williamson\TPLinkSmartplug;
 
 use InvalidArgumentException;
+use IPTools\Range;
 
 class TPLinkManager
 {
     protected $config;
     protected $devices;
 
-    public function __construct(array $config)
+    public function __construct(array $config = [])
     {
         $this->config = $config;
     }
@@ -20,11 +21,33 @@ class TPLinkManager
             throw new InvalidArgumentException('You have not setup the details for a device named ' . $name);
         }
 
-        return $this->newTPLinkDevice($this->config[$name], $name);
+        return new TPLinkDevice($this->config[$name], $name);
     }
 
     protected function newTPLinkDevice($config, $name)
     {
+        $this->config[$name] = $config;
         return new TPLinkDevice($config, $name);
+    }
+
+    public function deviceList()
+    {
+        return $this->config;
+    }
+
+    public function autoDiscoverTPLinkDevices($ipRange, $timeout = 1)
+    {
+        $ips = Range::parse($ipRange);
+        foreach($ips AS $ip){
+            $device = new TPLinkDevice(['ip' => $ip, 'port' => 9999, 'timeout' => $timeout], 'autodiscovery');
+            try{
+                $systemInfo = json_decode($device->sendCommand(TPLinkCommand::systemInfo()));
+                $this->newTPLinkDevice([
+                    'ip' => (string)$ip,
+                    'port' => 9999,
+                    'systemInfo' => $systemInfo->system->get_sysinfo
+                ], $systemInfo->system->get_sysinfo->alias);
+            } catch(\UnexpectedValueException $e) {}
+        }
     }
 }
