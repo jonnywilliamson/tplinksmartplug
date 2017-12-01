bfb0fe38d5caec827a46f7dcbb28edd997e8ad0a
Shane Rutter <shane@shanerutter.co.uk>
2017-12-01 08:52:22 +0000

    Laravel config updated with timeout setting

--- src/Laravel/config/TPLink.php
+++ src/Laravel/config/TPLink.php
@@ -8,11 +8,13 @@ return [
 //    'bedroom' => [
 //        'ip'   => '192.168.1.100', //Or hostname
 //        'port' => '9999',
+//        'timeout' => 5
 //    ],
 
 //    'livingroom' => [
 //        'ip'   => '192.168.1.101', //Or hostname
 //        'port' => '9999',
+//        'timeout' => 10
 //    ],
 
 ];
